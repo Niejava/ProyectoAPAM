2a6012d4957d0d5345b76c3c7de641167557f256
Alberto Arana <aranaboza@gmail.com>
2019-05-19 11:02:47 +0200

    Added dynamic plants selector

--- src/main.js
+++ src/main.js
@@ -20,7 +20,20 @@ function loadPlants() {
   xmlhttpPlants.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       plants = JSON.parse(this.responseText);
-      console.log(plants);
+
+      var elChangePlant = document.querySelector('.container-change-plant .row');
+
+      console.log(elChangePlant)
+      plants.forEach(function(p) {
+        var col = document.createElement('div');
+        col.classList.add('col');
+        col.innerHTML = `
+          <a href="/?plant=${p.type}">
+              <img class="img_${p.type} rounded mx-auto d-block img-thumbnail" src="assets/img/${p.img}" alt="${p.type}">
+          </a>
+        `;
+        elChangePlant.appendChild(col);
+      })
     }
   };
   xmlhttpPlants.open("GET", urlPlants, true);
